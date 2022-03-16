85257978b259f4cbd3aa9e6cb3bbe8e1ef296713
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-03-16 16:01:57 +0200

    calculator in python
    it can do 5 operations, which i will not explain so just look at the code, you'll probably know everything when you look at the code

---
+++ calculator.py
@@ -0,0 +1,60 @@
+exitornah = True
+
+while exitornah == True:
+
+    welcum = input("welcome to calculator, y to keep calculating and n to exit    ")
+
+    if welcum == "n":
+        print("fuck you for not using my calculator")
+        exitornah = False
+        break
+    if welcum == "y":
+        print(" ")
+
+    else:
+        print("i told you to use y and n dumb bitch")
+        exitornah = False
+        break
+
+    num1str = input("what is the first number?    ")
+    try:
+        num1int = int(num1str)
+    except:
+        print("error not a number, fuck you")
+        exitornah = False
+        break
+
+    num2str = input("what is the second number?    ")
+    try:
+        num2int = int(num2str)
+    except:
+        print("error not a number, fuck you")
+        exitornah = False
+        break
+
+    operator = input("choose an operator    ")
+    operatorstr = str(operator)
+    if '+' not in operatorstr:
+        if '-' not in operatorstr:
+            if '*' not in operatorstr:
+                if '/' not in operatorstr:
+                    if '=' not in operatorstr:
+                        exitornah = False
+                        break
+
+    if operatorstr == '+':
+        print(num1int, "+", num2int, "=", num1int+num2int)
+
+    if operatorstr == '-':
+        print(num1int, "-", num2int, "=", num1int-num2int)
+
+    if operatorstr == '*':
+        print(num1int, "*", num2int, "=", num1int*num2int)
+
+    if operatorstr == '/':
+        print(num1int, "/", num2int, "=", num1int/num2int)
+
+    if operatorstr == '=':
+        print(num1int, "=", num2int, "=", num1int==num2int)
+
+    print("sussy wussy")
